631f36d7c1035a5ec3bf43aad535d9201ee8067c
Jefferson Garcia <jemefox@hotmail.com>
2020-09-02 13:09:07 -0300

    Jquery

--- Front-End/JavaScript/JQuery/js/function.js
+++ Front-End/JavaScript/JQuery/js/function.js
@@ -9,3 +9,20 @@ $(function(){
 	alert("Seja Bem-Vindo!");
 })
 */
+
+$(function() {
+	/*
+    var el = $('.box');
+	
+	el.html('<div class="test"> Meu Elemento!</div>');
+	el.html('<h1 class="text1">Meu texto em javascript!</h1>');
+	
+	
+	$('.box2').text("<div></div>");
+	$('.box2').text("Olá Mundo!");
+	*/
+	$('input[type=button]').click(function(){
+		var v = $('input[type=text]').var();
+		var var2 = v.split("@");
+	})
+});
