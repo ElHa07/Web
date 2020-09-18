1a3d155121bfb75c49e43babf1efafc97d29c261
Jefferson Garcia <jemefox@hotmail.com>
2020-09-18 20:51:19 -0300

    .RhemaCODE

---
+++ Front-End/React/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>React JS</title>
+<script src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+</head>
+<body>
+  <div id="main"></div><!--main-->
+  <script src="file.babel" type="text/babel"></script>
+
+</body>
+</html>
